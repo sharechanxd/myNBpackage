7bc0ff84e3ba4774292790a6e452c6d4ebd11cb3
Kimsley Chen <xiaochangaga@outlook.com>
2021-11-22 23:12:08 -0500

    test

---
+++ R/nb.R
@@ -0,0 +1,15 @@
+
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rPackageTutorial)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
